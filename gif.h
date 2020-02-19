d1c9178717220fb7c06f257d2fc1def8ddb73326
Philippe Patti <43197@etu.he2b.be>
2020-02-19 12:47:22 +0100

    Printing header, width and height in hex by bytes

--- gif.h
+++ gif.h
@@ -1,13 +1,14 @@
 /**
  * prototype of logical screen descriptor structure :
  */
-typedef struct header_lsd_t 
+typedef struct __attribute__((__packed__)) header_lsd_t 
 {
-	char header[6];
-	char width[2];
+	unsigned char header[6];
+	unsigned char width[2];
 	char height[2];
 	char packed_field;
 	char data[2];
-}__attribute__((packed)) header_lsd_t;
+} header_lsd_t;
 
  
+
--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 
 #include "gif.h"
 
+void printBytes(const char* title, const char * string, size_t size);
+
 int main(int argc, char* argv[])
 {
   printf("%s\n", "Start program");
@@ -11,15 +13,44 @@ int main(int argc, char* argv[])
 	  return 0;
   }
   
-  FILE * gifSrc = fopen(argv[1], "rb");
+  FILE * gif_src = fopen(argv[1], "rb");
   errno = 0;
   
-  if(gifSrc == NULL) {
+  if(gif_src == NULL) {
 	  printf("\n");
 	  printf("File \"%s\" doesn't seem to exist. Please check filepath and spelling. --\n", argv[1]);
 	  return 0;
   }
 
   header_lsd_t header_lsd;
-  	
+  
+  fread(&header_lsd, sizeof(char), sizeof(header_lsd_t), gif_src);
+  printf("sizeof header_lsd %d\n", sizeof(header_lsd));
+  printf("sizeof header, width, length : %d , %d, %d \n",sizeof(header_lsd.header), sizeof(header_lsd.width), sizeof(header_lsd.height));
+  printf("sizeof char %d\n", sizeof(char));
+  printf("sizeof unsigned char %d\n", sizeof(unsigned char));
+
+ 
+  printBytes("Header", header_lsd.header, sizeof(header_lsd.header));
+  printf("Logical Screen Descriptor\n");
+  printBytes("Width", &header_lsd.width, sizeof(header_lsd.width));
+  printBytes("Height", &header_lsd.height, sizeof(header_lsd.height));
 }
+
+
+void printBytes(const char * title, const char * string, size_t size){
+	printf("%s\n",title);
+	unsigned char *p = (unsigned char*) string;
+	printf("strlen(string) = %d\n", strlen(string));
+	printf("size = %d\n", size);
+	for(int i = 0; i < size; ++i){
+		if(! (i%8) && i)
+			printf("\n");
+		printf("0x%0x ", p[i]);
+	}
+	printf("\n\n");
+}
+
+
+
+

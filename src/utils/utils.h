662ddf80ca577db0cd3aaee6e8cc1235b9e21e44
Philippe Patti <43197@etu.he2b.be>
2020-05-07 11:14:27 +0200

    Usage fixed for enc and dec

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define _PROGRAM_ "Steganography by LSB substitution\n"
+//TODO usage for dec
+#define _PROGRAM_USAGE "Usage %s : \n enc <source> <dest> <input text>\t enc : Encode text in dest with source image\n dec <source> <dest text>\t\t dec : Decode text in dest from source image\n"
 #define MODE_ENC "enc"
 #define MODE_DEC "dec"
-#define _PROGRAM_ "Steganograph by LSB substitution\n"
-//TODO usage for dec
-#define _PROGRAM_USAGE "Usage: %s [enc][dec] <source> <dest> <input text>\n enc : Write your text TO image\n dec : Read your hidden text FROM image\n"
 #define _ERROR_NB_ARGS_ "Argument number error!\n"
 #define _ERROR_UNKNOWN_MODE_ "Unknown mode!\n"
 #define _ERROR_OPEN_FILE_R "Impossible to open input file %s!\n"

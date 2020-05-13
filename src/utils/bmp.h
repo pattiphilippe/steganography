c6ec762f71f3ca5b1cc2a351b0904f7ee3fc2f7c
hind <42835@etu.he2b.be>
2020-05-13 18:40:47 +0200

    fin du rapport

--- src/utils/bmp.h
+++ src/utils/bmp.h
@@ -9,7 +9,7 @@ unsigned decode_length_bmp(FILE *src_img);
 void decode_secret_bmp(FILE *src_img, FILE *dest, const unsigned length);
 
 // BIT FUNCTIONS
-#define LSB 1 // 1 : using LSB method, 0 : using MSB method
+#define LSB 0// 1 : using LSB method, 0 : using MSB method
 int get_bit(char byte, int i);
 void hide_bit(FILE *src_img, FILE *dest, const int secret_bit);
 int decode_bit(FILE *src_img);
